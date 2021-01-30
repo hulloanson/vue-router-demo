0c142c4cd67ce2c6547a5cbe4ef8d4e03e098200
hulloanson <hulloanson@gmail.com>
2021-01-30 09:57:25 +0800

    hm whatever

---
+++ src/views/AsyncID.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="asyncid">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  mounted() {
+      console.log(this.$route.params)
+  },
+}
+</script>
